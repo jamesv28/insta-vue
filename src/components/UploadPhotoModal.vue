41aad7eb51e059213d15efb51784da14c2b3856d
James Volmert <james.volmert@gmail.com>
2024-03-22 18:01:19 -0600

    modal for uploading images

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 const open = ref(false);
-
+const caption = ref('')
+const file = ref('')
 const showModal = () => {
   open.value = true;
 };
@@ -11,6 +12,9 @@ const uploadPHoto = () => {
 
 }
 
+const handleUploadChange = (e) => {
+  if(e.target.files[0]) file.value = e.target.files[0]
+}
 const handleOk = (e) => {
   console.log(e);
   open.value = false;
@@ -21,7 +25,16 @@ const handleOk = (e) => {
     <div>
         <a-button type="" @click="uploadPHoto">Upload Photo</a-button>
         <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
-        
+          <input type="file" accept=".jpeg, .jpg, .png" required @change="handleUploadChange">
+          <input type="text" placeholder="Caption" required maxlength="50" v-model="caption"/>
         </a-modal>
   </div>
 </template>
+
+<style scoped>
+input {
+  display: block;
+  margin-top: 10px;
+  width: 100%;
+}
+</style>
